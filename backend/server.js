63f11a1e8e03e293f51134dadba3a7ac2207caae
Kartik Pal <mail.kartik2608@gmail.com>
2026-04-29 18:37:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("🔥 Backend is working!");
 });
-
+<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+<div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="kartikpal2608" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/kartikpal2608?trk=profile-badge">Kartik Pal</a></div>
+              
 app.get("/test", (req, res) => {
   res.json({ message: "API test successful" });
 });
